3567912c829d877e611e6d91e432015b2b9890a3
Aaron Williamson & Angela Yen <student@epicodus.com>
2014-02-24 14:06:47 -0800

    test that the right people are commiting

--- TriangleTracker-2.24/js/scripts.js
+++ TriangleTracker-2.24/js/scripts.js
@@ -1,17 +1,22 @@
 var Triangle = {
   invalid: function() {
     if (this.sideOne >= this.sideTwo + this.sideThree || this.sideTwo >= this.sideOne + this.sideThree || this.sideThree >= this.sideOne + this.sideTwo) {
+      alert("invalid triangle measurements");
       return false;
     }
   },
 
   type: function() {
-      if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
-        return "equilateral";
-      } else if (this.sideOne === this.sideTwo || this.sideTwo === this.sideThree || this.sideOne === this.sideThree) {
-        return "isosceles";
-      } else if (this.sideOne !== this.sideTwo && this.sideTwo !== this.sideThree && this.sideOne !== this.sideThree) {
-        return "scalene";
-      }
+    if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
+      return "equilateral";
+    } else if (this.sideOne === this.sideTwo || this.sideTwo === this.sideThree || this.sideOne === this.sideThree) {
+      return "isosceles";
+    } else if (this.sideOne !== this.sideTwo && this.sideTwo !== this.sideThree && this.sideOne !== this.sideThree) {
+      return "scalene";
     }
+  },
+
+  list: function() {
+    return this.sideOne + ", " + this.sideTwo + ", " + this.sideThree;
+  }
 };
