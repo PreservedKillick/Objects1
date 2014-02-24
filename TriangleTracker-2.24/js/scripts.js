c29ac67b319c1be3df876188171e8cce396adfd0
Aaron Williamson & Angela Yen <student@epicodus.com>
2014-02-24 14:46:13 -0800

    final draft objects triangles

--- TriangleTracker-2.24/js/scripts.js
+++ TriangleTracker-2.24/js/scripts.js
@@ -1,8 +1,7 @@
 var Triangle = {
   invalid: function() {
     if (this.sideOne >= this.sideTwo + this.sideThree || this.sideTwo >= this.sideOne + this.sideThree || this.sideThree >= this.sideOne + this.sideTwo) {
-      alert("invalid triangle measurements");
-      return false;
+      return true;
     }
   },
 
@@ -20,3 +19,44 @@ var Triangle = {
     return this.sideOne + ", " + this.sideTwo + ", " + this.sideThree;
   }
 };
+
+$(document).ready(function() {
+  $("form#triangle-sides").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSideOne = $("input#side-one").val();
+    var inputtedSideTwo = $("input#side-two").val();
+    var inputtedSideThree = $("input#side-three").val();
+
+    var newTriangle = Object.create(Triangle);
+    newTriangle.sideOne = inputtedSideOne;
+    newTriangle.sideTwo = inputtedSideTwo;
+    newTriangle.sideThree = inputtedSideThree;
+
+    if (newTriangle.invalid() === true) {
+      alert("Enter the right size dummy!");
+    } else if (newTriangle.type() === "equilateral") {
+      $('ul#equilateral').append("<li>" + newTriangle.list() + "</li>");
+    } else if (newTriangle.type() === "isosceles") {
+      $('ul#isosceles').append("<li>" + newTriangle.list() + "</li>");
+    } else if (newTriangle.type() === "scalene") {
+      $('ul#scalene').append("<li>" + newTriangle.list() + "</li>");
+    }
+    this.reset();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
